6ba7eaa391163a4ab3fc366fa04d87e3037bf122
dkarmy12 <55491427+dkarmy12@users.noreply.github.com>
2021-01-08 13:38:46 +0530

    Attempt to correct

--- app/main/utils.py
+++ app/main/utils.py
@@ -5,17 +5,16 @@ slot_dict = {0:{9:"A", 10:"B", 11:"C", 12:"D", 14:"P", 16:"Q",23:"D"},
             1:{9:"D", 10:"E", 11:"F", 12:"G", 14:"R", 16:"S"},
             2:{9:"B", 10:"C", 11:"A", 12:"G", 14:"F"},
             3:{9:"C", 10:"A", 11:"B", 12:"E", 14:"Q", 16:"P"},
-            4:{9:"E", 10:"F", 11:"D", 12:"G", 14:"S", 16:"R"}}
+            4:{9:"E", 10:"F", 11:"D", 12:"G", 14:"S", 16:"R",13:"B"}}
 
 def get_slot(dt_object):
     """Function to return the string slot using data passed from a datetime.datetime object"""
     accessor = dt_object.hour
     day = dt_object.weekday()
-    try :
-        slot = slot_dict[day][accessor]
-    except KeyError as e:
+    if accessor in slot_dict[day]:
+        return slot_dict[day][accessor]
+    else:
         return None
-    return slot
 
 def is_valid_link(url):
     # Validates for google meet
